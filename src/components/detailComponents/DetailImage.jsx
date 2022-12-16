bc7032aab1e5d128191f14f80aa500a526fb2e2c
wspenc <105823133+wspenc@users.noreply.github.com>
2022-12-16 15:06:47 -0700

    Update DetailImage.jsx

--- src/components/detailComponents/DetailImage.jsx
+++ src/components/detailComponents/DetailImage.jsx
@@ -6,13 +6,11 @@ const DetailImage = ({ image, title }) => {
   const backgroundString = `--background: url(${image})`
   return (
     <div
-      // className={moreStyles.ad_banner}
       className={styles.banner}
       style={{
         backgroundSize: "cover",
         background: `linear-gradient(190deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${image})`
       }}
-    // style={{backgroundString}}
     >
       <div className={styles.ad_text}>
         <h1>{title}</h1>
